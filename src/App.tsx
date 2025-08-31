ca7edd6bbfd4b47b76096896457d765a0a6a6376
dbbhuh5 <dbbhuh5@gmail.com>
2025-08-31 05:18:35 +0000

    fix
    set  as default ripple color

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <button className='md-ripples ripples-dark' style={{ padding: 0, borderRadius: "999px", overflow: "hidden" }} onClick={() => { console.log("clicked");
+      <button className='md-ripples' style={{ padding: 0, borderRadius: "999px", overflow: "hidden" }} onClick={() => { console.log("clicked");
       }}>
         <span style={{ width: "200px", height: "100px", display: "flex", justifyContent: "center", alignItems: "center", userSelect: "none" }}>Click me</span>
       </button>
